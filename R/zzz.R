9d4e041a37ff57b16ba2856793af79bf3672fc9f
wds <wds8.24@qq.com>
2024-10-18 17:17:33 +0800

    file url

--- R/zzz.R
+++ R/zzz.R
@@ -1,40 +1,55 @@
-.onLoad <- function(libname, pkgname) {
-	extdata <- system.file("extdata", package=pkgname,
-			lib.loc=libname, mustWork=TRUE)
-	
-	if(!file.exists(file.path(extdata, "snp138.db"))) {
-		cat("downloading and installing SNP database of GRCh37/hg19...\n")
-		download(url="https://github.com/WWz33/snpGeneSets/blob/main/inst/extdata/snp138.zip", 
-				destfile=file.path(extdata, "snp138.zip"))
-		unzip(file.path(extdata, "snp138.zip"), exdir = extdata)
-		if(file.exists(file.path(extdata, "snp138.zip")))
-			file.remove(file.path(extdata, "snp138.zip"))
-	} 
-	if(!file.exists(file.path(extdata, "snp142.db"))) {
-		cat("downloading and installing SNP database of GRCh38/hg38...\n")
-		download(url="https://github.com/WWz33/snpGeneSets/blob/main/inst/extdata/snp142.zip", 
-				destfile=file.path(extdata, "snp142.zip"))
-		unzip(file.path(extdata, "snp142.zip"), exdir = extdata)
-		if(file.exists(file.path(extdata, "snp142.zip")))
-			file.remove(file.path(extdata, "snp142.zip"))
-	}
-	if(!file.exists(file.path(extdata, "gene105.db"))) {
-		cat("downloading and installing Gene database of GRCh37/hg19...\n")
-		download(url="https://github.com/WWz33/snpGeneSets/blob/main/inst/extdata/gene105.zip", 
-				destfile=file.path(extdata, "gene105.zip"))
-		unzip(file.path(extdata, "gene105.zip"), exdir = extdata)
-		if(file.exists(file.path(extdata, "gene105.zip")))
-			file.remove(file.path(extdata, "gene105.zip"))	
-	}
-	if(!file.exists(file.path(extdata, "gene106.db"))) {
-		cat("downloading and installing Gene database of GRCh38/hg38...\n")
-		download(url="https://github.com/WWz33/snpGeneSets/blob/main/inst/extdata/gene106.zip", 
-				destfile=file.path(extdata, "gene106.zip"))
-		unzip(file.path(extdata, "gene106.zip"), exdir = extdata)
-		if(file.exists(file.path(extdata, "gene106.zip")))
-			file.remove(file.path(extdata, "gene106.zip"))
-	}
-	if(!file.exists(file.path(extdata, "msigdb.db"))) {
-		cat("MSigDB database is not existing, please use msigdb_build function to install msigdb gene sets first!\n")
-	}
+if (!file.exists(file.path(extdata, "snp138.db"))) {
+  cat("downloading and installing SNP database of GRCh37/hg19...\n")
+  tryCatch({
+    download.file(url = "https://github.com/WWz33/snpGeneSets/blob/main/inst/extdata/snp138.zip", 
+                  destfile = file.path(extdata, "snp138.zip"), method = "curl", quiet = TRUE)
+    unzip(file.path(extdata, "snp138.zip"), exdir = extdata)
+    if (file.exists(file.path(extdata, "snp138.zip"))) {
+      file.remove(file.path(extdata, "snp138.zip"))
+    }
+  }, error = function(e) {
+    cat(sprintf("Error downloading snp138.zip: %s\n", e$message))
+  })
+}
+
+if (!file.exists(file.path(extdata, "snp142.db"))) {
+  cat("downloading and installing SNP database of GRCh38/hg38...\n")
+  tryCatch({
+    download.file(url = "https://github.com/WWz33/snpGeneSets/blob/main/inst/extdata/snp142.zip", 
+                  destfile = file.path(extdata, "snp142.zip"), method = "curl", quiet = TRUE)
+    unzip(file.path(extdata, "snp142.zip"), exdir = extdata)
+    if (file.exists(file.path(extdata, "snp142.zip"))) {
+      file.remove(file.path(extdata, "snp142.zip"))
+    }
+  }, error = function(e) {
+    cat(sprintf("Error downloading snp142.zip: %s\n", e$message))
+  })
+}
+
+if (!file.exists(file.path(extdata, "gene105.db"))) {
+  cat("downloading and installing Gene database of GRCh37/hg19...\n")
+  tryCatch({
+    download.file(url = "https://github.com/WWz33/snpGeneSets/blob/main/inst/extdata/gene105.zip", 
+                  destfile = file.path(extdata, "gene105.zip"), method = "curl", quiet = TRUE)
+    unzip(file.path(extdata, "gene105.zip"), exdir = extdata)
+    if (file.exists(file.path(extdata, "gene105.zip"))) {
+      file.remove(file.path(extdata, "gene105.zip"))
+    }
+  }, error = function(e) {
+    cat(sprintf("Error downloading gene105.zip: %s\n", e$message))
+  })
+}
+
+if (!file.exists(file.path(extdata, "gene106.db"))) {
+  cat("downloading and installing Gene database of GRCh38/hg38...\n")
+  tryCatch({
+    download.file(url = "https://github.com/WWz33/snpGeneSets/blob/main/inst/extdata/gene106.zip", 
+                  destfile = file.path(extdata, "gene106.zip"), method = "curl", quiet = TRUE)
+    unzip(file.path(extdata, "gene106.zip"), exdir = extdata)
+    if (file.exists(file.path(extdata, "gene106.zip"))) {
+      file.remove(file.path(extdata, "gene106.zip"))
+    }
+  }, error = function(e) {
+    cat(sprintf("Error downloading gene106.zip: %s\n", e$message))
+  })
 }
